9386e8fe7a67d92955de9e1d733ec49cbc5a70cb
Niklas Bölter <bot811@t-online.de>
2020-12-04 20:04:22 +0100

    refactored to use await and try/catch

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ const port = process.env.PORT || 5000
 const mongodbConnectionString = process.env.MONGODB_CONNECTION_STRING || ''
 const mongodbCollection = process.env.MONGODB_COLLECTION || ''
 
+if (mongodbConnectionString.length == 0) {
+  throw new Error('Set Environment Variable MONGODB_CONNECTION_STRING')
+}
+if (mongodbCollection.length == 0) {
+  throw new Error('Set Environment Variable MONGODB_COLLECTION')
+}
 
 const weeknumImageSchema = new mongoose.Schema({
   weeknum: Number,
@@ -31,15 +37,13 @@ mongoose.connect(mongodbConnectionString, {
 })
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send('online')
 })
 
 app.get('/ocr', (req, res) => {
-  const weeknum = moment().week()
   try {
-    getImageOfWeeknum(weeknum)
-
-    res.status(200).send('ok')
+    handleOcr()
+    res.status(200).send('accepted')
   } catch (error) {
     console.log(error)
     res.status(500).send(error)
@@ -50,28 +54,41 @@ app.listen(port, () => {
   console.log(`Listening on ${port}`)
 })
 
-function getImageOfWeeknum(weeknumToRead) {
-  WeeknumImage.findOne({ weeknum: weeknumToRead }, function (err, result) {
-    if (err) {
-      throw err
-    }
-    if (result != null) {
-      base64StringToJpeg(result.jpegImageAsBase64String)
-      return
-    }
-    throw new Error('No results returned, is this weeknum already in database')
-  })
+async function handleOcr() {
+  const weeknum = moment().week()
+  console.log(weeknum)
+  try {
+    const jpegImageAsBase64String = await getImageOfWeeknum(weeknum)
+    console.log(jpegImageAsBase64String.length)
+
+    const filepathOfImage = await base64StringToJpeg(jpegImageAsBase64String)
+    console.log(filepathOfImage)
+
+  } catch (err) {
+    console.log(err)
+  }
 }
 
-function base64StringToJpeg(base64String) {
-  const tmpobj = tmp.fileSync()
-  const filename = tmpobj.name
-  console.log('Filename: ', filename);
+async function getImageOfWeeknum(weeknumToRead) {
+  const result = await WeeknumImage.findOne({ weeknum: weeknumToRead }).exec()
+  if (result != null) {
+    return result.jpegImageAsBase64String
+  }
+  throw new Error('No results returned, is this weeknum already in database?')
+}
 
-  const imageBufferData = Buffer.from(String(base64String), 'base64')
-  const streamObj = new stream.Readable()
-  streamObj.push(imageBufferData)
-  streamObj.push(null)
-  streamObj.pipe(fs.createWriteStream(filename))
-  return filename
+async function base64StringToJpeg(base64String) {
+  return new Promise((resolve, reject) => {
+    const tmpobj = tmp.fileSync()
+    const filename = tmpobj.name
+
+    const imageBufferData = Buffer.from(String(base64String), 'base64')
+    const streamObj = new stream.Readable()
+    streamObj.push(imageBufferData)
+    streamObj.push(null)
+    streamObj.pipe(fs.createWriteStream(filename))
+
+    streamObj.on('end', () => resolve(filename))
+    streamObj.on('error', (err) => reject(err))
+  })
 }
